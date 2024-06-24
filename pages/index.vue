0beb9a36f923531051723f917dbd6e4f106d70f4
gautchap <gauchap+github@outlook.com>
2024-06-24 14:11:53 +0200

    feat : create homepage with list of events

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const { data: events } = await useFetch("/api/events");
+</script>
+
+<template>
+    <div>
+        <h2>Events</h2>
+        <ul>
+            <li v-for="event in events">
+                <NuxtLink :to="`/event/${event.id}`">{{ event.name }}</NuxtLink>
+            </li>
+        </ul>
+    </div>
+</template>
