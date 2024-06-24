defc316766fefc9ce28f5c567de002d45dbe8176
gautchap <gauchap+github@outlook.com>
2024-06-24 21:48:43 +0200

    refactor : filter events by date

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
 const { data: events } = await useFetch("/api/events");
+useSeoMeta({
+    title: "UFC events",
+});
 </script>
 
 <template>
     <div>
-        <h2>Events</h2>
+        <h2>Past Events</h2>
         <ul>
-            <li v-for="event in events">
+            <li v-for="event in events?.pastEvents">
+                <NuxtLink :to="`/event/${event.id}`">{{ event.name }}</NuxtLink>
+            </li>
+        </ul>
+    </div>
+    <div>
+        <h2>Future Events</h2>
+        <ul>
+            <li v-for="event in events?.futurEvents">
                 <NuxtLink :to="`/event/${event.id}`">{{ event.name }}</NuxtLink>
             </li>
         </ul>
--- server/api/events.get.ts
+++ server/api/events.get.ts
@@ -1,6 +1,8 @@
 import { getUFCEvents } from "@/utils/events";
+import { getFormatEvent } from "@/utils/format-event";
 
 export default defineCachedEventHandler(async () => {
     const events = await getUFCEvents();
-    return events;
+    const { futurEvents, pastEvents } = getFormatEvent(events);
+    return { futurEvents, pastEvents };
 });
