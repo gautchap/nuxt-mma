7ffde07a625cdf8b6b01e091eb186411be47c051
gautchap <gauchap+github@outlook.com>
2024-06-24 21:49:32 +0200

    refactor: change the event page title and add a back button

--- pages/event/[eventId].vue
+++ pages/event/[eventId].vue
@@ -5,9 +5,14 @@ const route = useRoute();
 const { data: event } = await useFetch(`/api/event/${route.params.eventId}`, {
     transform: (res) => eventSchema.parse(res),
 });
+
+useSeoMeta({
+    title: event?.value?.name,
+});
 </script>
 
 <template>
+    <NuxtLink to="/">← Retour</NuxtLink>
     <div v-if="event === null">L'événement n'existe pas</div>
     <Event v-if="event !== null" :event="event" />
 </template>
