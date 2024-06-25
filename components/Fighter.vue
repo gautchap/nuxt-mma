f875c697b20a54edb481b96cab116f52c9b9873b
gautchap <gauchap+github@outlook.com>
2024-06-25 14:35:16 +0200

    refactor : display more infos about fighter

--- components/Fighter.vue
+++ components/Fighter.vue
@@ -7,12 +7,46 @@ const props = defineProps<{
 </script>
 
 <template>
-    <p :class="props.fighter.winner ? 'winner' : ''">{{ props.fighter.athlete.fullName }}</p>
+    <div class="card">
+        <div class="card-header">
+            <p class="card-title">{{ props.fighter.athlete.fullName }}</p>
+            <NuxtImg
+                :src="props.fighter.athlete.flag.href"
+                :alt="props.fighter.athlete.flag.alt"
+                width="24"
+                height="24"
+            />
+        </div>
+        <NuxtImg
+            class="profile"
+            :class="props.fighter.winner ? 'winner' : ''"
+            provider="espn"
+            :src="`/${props.fighter.id}.png`"
+            :alt="props.fighter.athlete.fullName"
+            width="132.11"
+            height="96"
+        />
+    </div>
 </template>
 
 <style scoped>
+.card-header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.card {
+    display: flex;
+    flex-direction: column;
+}
+
 .winner {
-    background-color: green;
-    color: white;
+    border: 2px solid green;
+}
+.profile {
+    width: 8em;
+    margin-left: auto;
+    margin-right: auto;
 }
 </style>
